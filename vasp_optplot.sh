09ac566f66713768ed67c95ee99f4183db17e8c9
Jan Mewes (humboldt <mewes@ctcp-cluster.cm.cluster>
2018-11-29 09:12:01 +1300

    :wnew scripts from ctc n

--- vasp_optplot.sh
+++ vasp_optplot.sh
@@ -29,19 +29,19 @@ for i in $(seq 2 $bigstep $tostep)
 	do energy=$(grep " $i F=" output | tail -n1 | cut -c 29-41)
 	   energy_eH=$(echo $energy | sed s/[eE]+*/*10^/ | bc | cut -c -9)
 	   dE=$(grep " $i F=" output | tail -n1 | cut -c 49-)
-	   dE1=$(echo "27.2114*$dE" | sed 's/[eE]+*/*10^/' | bc -l| cut -c -10)
+	   dE1=$(echo "$dE" | sed 's/[eE]+*/*10^/' | bc -l| cut -c -10)
 		echo -e "$i\t $energy_eH\t $dE1"
 	done
 for i in $(seq $start $atstep) 
 	do energy=$(grep " $i F=" output | tail -n1 | cut -c 29-41)
 	   energy_eH=$(echo $energy | sed s/[eE]+*/*10^/ | bc | cut -c -9)
 	   dE=$(grep " $i F=" output | tail -n1 | cut -c 49-)
-	   dE1=$(echo "27.2114*$dE" | sed 's/[eE]+*/*10^/' | bc -l | cut -c -10)
+	   dE1=$(echo "$dE" | sed 's/[eE]+*/*10^/' | bc -l | cut -c -10)
 		echo -e "$i\t $energy_eH\t $dE1"
 	done
 
 Econv=$(grep EDIFFG INCAR | awk '{print $3}' | sed 's/[eE]+*/*10^/' | bc -l)
-echo -e "      Converged when dE < "$(echo "scale=6;$Econv*27.2114" | bc | cut -c -9)
+echo -e "      Converged when dE < "$(echo "scale=6; 27.2114*$Econv" | bc | cut -c -9)
 	 
 
 		
